8cff619bdefa8cf85d0a6b5ce64e8dc5a84beb1f
Gicelly Oliveira <gicelly.oliveira@gmail.com>
2022-11-22 00:23:02 -0300

    pagina carrinho

--- src/pages/Carrinho/Carrinho.js
+++ src/pages/Carrinho/Carrinho.js
@@ -1,12 +1,28 @@
-import styles from './Carrinho.module.scss';
-import { Header } from '../../components/Header/Header';
+import styles from "./Carrinho.module.scss";
+import { Header } from "../../components/Header/Header";
 
 const Carrinho = () => {
-  return (
-    <>
-      <Header titleCategory={'Finalize sua compra agora!'} description={'Todos os jogos com 10% de desconto'} />
-    </>
-  )
-}
+	return (
+		<>
+			<Header
+				titleCategory={"Finalize sua compra agora!"}
+				description={"Todos os jogos com 10% de desconto"}
+			/>
+			<div className={styles.container}>
+				<div className={styles.item1}></div>
+				<div className={styles.item2}>
+					<div className={styles.divResumo}>
+						<span className={styles.bold}>Resumo da compra</span>
+						<div>
+							<span>Subtotal: </span>
+							<span className={styles.bold}>R$ 0.00</span>
+						</div>
+					</div>
+					<button>Finalizar compra</button>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default Carrinho
+export default Carrinho;
